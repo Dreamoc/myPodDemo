5fb9dfc0dc625c3ea447c9c3ea5cf15bab4e0a58
fengjinlin <461775260@qq.com>
2020-11-25 16:18:21 +0800

    添加项目

---
+++ myPodDemo/myPodDemo/myPodDemo.h
@@ -0,0 +1,18 @@
+//
+//  myPodDemo.h
+//  myPodDemo
+//
+//  Created by Dreamoc on 2020/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myPodDemo.
+FOUNDATION_EXPORT double myPodDemoVersionNumber;
+
+//! Project version string for myPodDemo.
+FOUNDATION_EXPORT const unsigned char myPodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myPodDemo/PublicHeader.h>
+
+
